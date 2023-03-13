b3aaebf0b7d7ac1361c6f8d061f4f0a40977115f
Clark Rasmussen <cjr@clarkrasmussen.com>
2023-03-13 11:51:27 -0400

    Make exception more verbose

--- src/TrelloApi.php
+++ src/TrelloApi.php
@@ -61,7 +61,8 @@ class TrelloApi
 		// DECODE THE RESPONSE INTO A GENERIC OBJECT
 		$data = json_decode($response);
 		if (json_last_error() !== JSON_ERROR_NONE) {
-			throw new RuntimeException('API response was not valid JSON');
+			$msg = 'API response "' . $response . '" was not valid JSON';
+			throw new RuntimeException($msg);
 		}
 
 		return $data;
